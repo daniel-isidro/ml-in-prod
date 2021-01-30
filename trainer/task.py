b81d7db9c7720caf2a80720979cbe487ca1f34fe
Daniel Isidro <daniel.isidro@mac.com>
2021-01-30 10:18:01 +0000

    Final code

--- trainer/task.py
+++ trainer/task.py
@@ -4,6 +4,7 @@ import argparse
 import logging.config
 import os
 import time
+import sys
 
 import tensorflow as tf
 
@@ -32,9 +33,11 @@ def _download_data():
     return x_train, y_train, x_test, y_test
 
 
-def _preprocess_data(x, y):
+def _preprocess_data(x, y, needs_reshape):
     LOGGER.info("Transforming data")
     x = x / 255.0
+    if needs_reshape:
+        x = x.reshape(-1, 28, 28, 1)
     y = utils.to_categorical(y)
     return x,y
 
@@ -51,6 +54,7 @@ def _build_model_dense():
 
     return m
 
+
 def _build_model_cnn():
     m = models.Sequential()
 
@@ -62,25 +66,32 @@ def _build_model_cnn():
     m.add(layers.Conv2D(8, (3, 3), activation=activations.relu))
     m.add(layers.MaxPooling2D((2, 2)))
     m.add(layers.Flatten())
-
     m.add(layers.Dense(10, activation=activations.softmax))
 
     return m
 
 
 def train_and_evaluate(batch_size, epochs, job_dir, output_path, is_hypertune, model_type):
+    
     # Download the data
     x_train, y_train, x_test, y_test = _download_data()
 
-    # Preprocess the data
-    x_train, y_train = _preprocess_data(x_train, y_train)
-    x_test, y_test = _preprocess_data(x_test, y_test)
-
+    needs_reshape = False
     # Build the model
     if model_type == 'dense':
         model = _build_model_dense()
-    else:
+        needs_reshape = False
+    elif model_type == 'cnn':
         model = _build_model_cnn()
+        needs_reshape = True
+    else:
+        LOGGER.error("Unknown model type %s" % model_type)
+        sys.exit(1)
+
+
+    # Preprocess the data
+    x_train, y_train = _preprocess_data(x_train, y_train, needs_reshape)
+    x_test, y_test = _preprocess_data(x_test, y_test, needs_reshape)
 
     model.compile(loss=losses.categorical_crossentropy,
                   optimizer=optimizers.Adam(),
@@ -91,14 +102,14 @@ def train_and_evaluate(batch_size, epochs, job_dir, output_path, is_hypertune, m
     # e.g. tensorboard --logdir gs://BUCKET/tmp/logs  <-- no slash
     logdir = os.path.join(job_dir, "logs/scalars/" + time.strftime("%Y%m%d-%H%M%S"))
     tb_callback = callbacks.TensorBoard(log_dir=logdir)
-    model.fit(x_train.reshape(-1, 28, 28,1), 
+    model.fit(x_train, 
               y_train, 
               epochs=epochs, 
               batch_size=batch_size, 
               callbacks=[tb_callback])
 
     # Evaluate the model
-    loss_value, accuracy = model.evaluate(x_test.reshape(-1, 28, 28,1), y_test)
+    loss_value, accuracy = model.evaluate(x_test, y_test)
     LOGGER.info("  *** LOSS VALUE:  %f     ACCURACY: %.4f" % (loss_value, accuracy))
 
     # Communicate the results of the evaluation of the model
@@ -118,21 +129,29 @@ def train_and_evaluate(batch_size, epochs, job_dir, output_path, is_hypertune, m
 def main():
     """Entry point for your module."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model-type', type=str, help='Selecting dense layers model or CNN layers model')
     parser.add_argument('--hypertune', action='store_true', help='This is a hypertuning job')
     parser.add_argument('--batch-size', type=int, help='Batch size for the training')
     parser.add_argument('--epochs', type=int, help='Number of epochs for the training')
     parser.add_argument('--job-dir', default=None, required=False, help='Option for AI Platform')
-    parser.add_argument('--model-output-path', help='Path to write the SaveModel format')
+    parser.add_argument('--model-output-path', help='Path to write the SaveModel format', default=None)
+    parser.add_argument('--model-type', help='Type of model to train', default='dense')
 
     args = parser.parse_args()
 
-    model_type = args.model_type
     is_hypertune = args.hypertune
     batch_size = args.batch_size
     epochs = args.epochs
     job_dir = args.job_dir
     output_path = args.model_output_path
+    model_type = args.model_type
+
+    if not model_type in ['dense', 'cnn']:
+        print('Model type must be dense or cnn')
+        sys.exit(1)
+
+    if (not is_hypertune) and output_path is None:
+        print('Please set --model-output-path')
+        sys.exit(1)
 
     train_and_evaluate(batch_size, epochs, job_dir, output_path, is_hypertune, model_type)
 
